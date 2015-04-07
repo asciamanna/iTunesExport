a686d47e1e7b8916d65012569c09cf09a6850392
Anthony Sciamanna <asciamanna@gmail.com>
2015-04-07 19:25:44 -0400

    updating album artwork.

---
+++ Updates.sql
@@ -0,0 +1,33 @@
+USE [dba55dc3fe9a5f44289460a1a1016fc0c7]
+GO
+
+delete from Album where Artist = 'Horace Silver Quintet' and Name = 'Horace-Scope'
+delete from Album where Name like 'Voice Memos%'
+update Album set ArtworkLocation = 'http://images.amazon.com/images/P/B000002UH6.01._SCMZZZZZZZ_.jpg' where Name like '%TV Town'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/3914093/UltraLounge+Volume+6+Rhapsodesia+Folder.jpg' where Name like '%Rhapsodesia'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/85897751/UltraLounge+Vol+7+Crime+Scene+cover_png.png' where Name like '%Crime Scene'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/58709901/UltraLounge++Bottoms+Up+Volume+Eighteen.jpg' where Name like '%Bottoms Up%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/90097605/UltraLounge++Bachelor+Pad+Royale++Volume+Four.jpg' where Name like '%Bachelor Pad Royale%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/91415683/UltraLounge+Wild+Cool++Swingin+Too+Volume+Fifteen.jpg' where Name like '%Wild, Cool%Too%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/98791895/UltraLounge+Vol+3+Space+Capades+UltraLounge+Vol+3+Space+Capade.png' where Name like '%Space Capades%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/3914069/UltraLounge+Volume+15+Wild+Cool++Swingin+Too+Folder.jpg' where Name like '%Wild Cool%Swingin'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/89639407/Tommy+Flanagan+Trio.jpg' where Name like 'Tommy Flanagan Trio'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/14179801/UltraLounge+Vegas+Baby+vegas+baby.jpg' where Name like '%Vegas Baby%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/74684872/Skyscraper+173132_1_f.jpg' where Name like 'Skyscraper'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/49336359/How+My+Heart+Sings+heartsings.jpg' where Name like 'How My Heart Sings%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/79768431/Final+Descent+Holy+Grail+From+Hell.jpg' where Name like 'Final Descent'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/90722381/Dehumanizer+PNG.png' where Name like 'Dehumanizer'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/54218757/Christmas+Break+Relaxing+Jazz+For+The+Holidays.jpg' where Name like '%Relaxing Jazz For The Holidays'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/92184557/A+Flock+Of+Seagulls.png' where Name like 'A Flock Of Seagulls%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/43464943/Music+for+Pets+63680.jpg' where Name like 'Music For Pets%'
+GO
+
+update Album set ArtworkLocation = 'http://ec2.images-amazon.com/images/P/B000002UFL.09._SCMZZZZZZZ_V1056633131_.jpg' where Name like '%Christmas Cocktails%One'
+GO
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/90100511/UltraLounge++Christmas+Cocktails+Volume+II.jpg' where Name like '%Christmas Cocktails%2'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/61352859/Curtain+Call+cover.jpg' where Name like '%Hank Mobley Quintet Featuring Sonny Clark'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/91674655/Anything+Goes.jpg' where Name like '%Anything Goes%'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/22962007/Time+In+THE+DAVE+BRUBECK+QUARTET++TIME.jpg' where Name like 'Time In (Remaster 2011)'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/91671245/My+Favorite+Things.jpg' where Name like 'My Favorite Things (Remaster 2011)'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/500/97796041/Angel+Eyes.jpg' where Name like 'Angel Eyes (Remaster 2011)'
+update Album set ArtworkLocation = 'http://userserve-ak.last.fm/serve/_/88868013/Jazz+Impressions+Of+Japan.png' where Name like 'Jazz Impressions of Japan%'
